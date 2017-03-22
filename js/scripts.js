02fefed1cd3cfd4cc6d5ec6fcd1c3afac57b7235
elinazhukov <elina@Elinas-MacBook-Pro.local>
2017-03-21 20:40:58 -0700

    Changed things in html, css, scripts, added simple weather and side plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,37 @@
-// YOUR SCRIPTS GO HERE
-// http://simpleweatherjs.com/
-
 // https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
 
+// SPOKANE weather
+
 $.simpleWeather({
-  location: 99004,
+    location: 'spokane',
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('.city').text(weather.city);
+      $('.temp').text(weather.temp);
+      $('i').addClass( 'icon-' + weather.code );
+      
+      // Get Condition Code
+      console.log(weather.code);
+            
+            
+      if ( weather.code >= 23 && weather.code <= 29 ) {
+         $('body').addClass('cloudy');   
+      }
+      if ( weather.code >= 30 && weather.code <= 36 ) {
+         $('body').addClass('sunny');   
+      }
+            
+          
+          
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
   
-  unit: 'f',
-  
-  success: function(weather){
-    // Check for weather
-    console.log(weather);
-    
-    // Display Weather
-    $('.temp').text(weather.temp);
-    $('.city').text(weather.city);
-    $('.state').text(weather.region);
-    $('.image img').attr('src', weather.image);
-    
-  },
-  
-  error: function(error) {
-    console.log('Look outside.');
-  }
-});
+  });
