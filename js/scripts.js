e042afaeea2ae7d890cf12ffddd2b4a1bd8eb5e2
elinazhukov <elina@Elinas-MacBook-Pro.local>
2017-03-22 10:21:33 -0700

    Added sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 // https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
 
+$('#sidr-show-hide').sidr({
+    timimg:'ease-in',
+	speed:500
+});
+
+$('#close').sidr({
+  method: 'close',
+	timimg:'ease-out',
+	speed:500
+});
+
+
+
+
 // SPOKANE Weather
 
 $.simpleWeather({
-    location: '098646',
+    location: 'spokane',
     unit: 'f',
     success: function(weather) {
       // Entire weather object
@@ -69,10 +83,7 @@ $.simpleWeather({
       }
       if ( weather.code >= 41 && weather.code <= 43 ) {
          $('div').addClass('gross');   
-      }
-          
-          
-          
+      }       
       
     },
     error: function(error) {
@@ -81,3 +92,4 @@ $.simpleWeather({
     }
   
   });
+
